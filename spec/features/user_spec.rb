1a89a2d5e994a15c079cf97877dddf5fb99227fe
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-08-31 15:00:08 -0300

    Extending user acceptance tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,12 +1,22 @@
 feature 'User' do
-  scenario 'creating new one' do
-    name = Faker::Name.name
-    email = Faker::Internet.email
+  let(:user) { FactoryGirl.build(:user) }
 
+  before(:all) do
+    User.destroy_all
+  end
+
+  scenario 'creating new one' do
     visit new_user_path
-    fill_in 'Name', with: name
-    fill_in 'Email', with: email
+    fill_in 'Name', with: user.name
+    fill_in 'Email', with: user.email
     click_on 'Save'
-    expect(page).to have_selector('li', text: name)
+    expect(page).to have_selector('li', text: user.name)
+  end
+
+  scenario 'deleting that user' do
+    user.save!
+    visit users_path
+    click_on 'Remove'
+    expect(page).to have_no_content user.email
   end
 end
